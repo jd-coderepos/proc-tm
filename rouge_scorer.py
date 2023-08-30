89141097abd5491bfe7cec25133b5c6993cc99dc
D'Souza <Jennifer.DSouza@tib.eu>
2023-08-30 11:01:00 +0200

    updated chatgpt response for rouge score

---
+++ rouge_scorer.py
@@ -0,0 +1,51 @@
+from rouge_score import rouge_scorer
+
+def read_files_into_list(filenames):
+    """
+    Read the contents of multiple files into a list.
+
+    Parameters:
+    - filenames (list): List of file paths to be read.
+
+    Returns:
+    - List of strings where each string is the content of a file.
+    """
+    contents = []
+    for filename in filenames:
+        with open("./photography/type1 - list prompts/ontology/"+filename, 'r', encoding="utf8") as file:
+            contents.append(file.read())
+    return contents
+
+# Example usage:
+filenames = ["goldstd-response-example1.txt", "goldstd-response-example2.txt", "goldstd-response-example3.txt"]
+references = read_files_into_list(filenames)
+
+filenames = ["chatgpt-response-example1.txt", "chatgpt-response-example2.txt", "chatgpt-response-example3.txt"]
+predictions = read_files_into_list(filenames)
+
+scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL', 'rougeLsum'])
+
+cumulative_scores = {
+    'rouge1': {'precision': 0, 'recall': 0, 'fmeasure': 0},
+    'rouge2': {'precision': 0, 'recall': 0, 'fmeasure': 0},
+    'rougeL': {'precision': 0, 'recall': 0, 'fmeasure': 0},
+    'rougeLsum': {'precision': 0, 'recall': 0, 'fmeasure': 0}
+}
+
+# Calculate ROUGE scores and accumulate
+num_samples = len(predictions)
+for prediction, reference in zip(predictions, references):
+    scores = scorer.score(reference, prediction)
+    for metric, values in scores.items():
+        cumulative_scores[metric]['precision'] += values.precision
+        cumulative_scores[metric]['recall'] += values.recall
+        cumulative_scores[metric]['fmeasure'] += values.fmeasure
+
+# Average the scores
+for metric, values in cumulative_scores.items():
+    cumulative_scores[metric]['precision'] /= num_samples
+    cumulative_scores[metric]['recall'] /= num_samples
+    cumulative_scores[metric]['fmeasure'] /= num_samples
+
+print(cumulative_scores)
+
